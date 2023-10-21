9d0cff69b3bf4a98629dfd47394875873ec6af3e
Elen Grigoryan <145226713+mynameistzuk@users.noreply.github.com>
2023-10-21 23:58:03 +0400

    Add files via upload
    Winner

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,20 +1,34 @@
-def table(a):
-    print(' ----- ----- -----')
-    print('|     |     |     |')
-    print(f'|  {a[0]}  |  {a[1]}  |  {a[2]}  |')
-    print('|     |     |     |')
-    print(' ----- ----- -----')
-    print('|     |     |     |')
-    print(f'|  {a[3]}  |  {a[4]}  |  {a[5]}  |')
-    print('|     |     |     |')
-    print(' ----- ----- -----')
-    print('|     |     |     |')
-    print(f'|  {a[6]}  |  {a[7]}  |  {a[8]}  |')
-    print('|     |     |     |')
-    print(' ----- ----- -----')
+def winner(table):
+    """
+    This is going to check the rows
+    """
+    for i in range(0, 9, 3):
+        if table[i] == table[i + 1] == table[i + 2] == X or O:
+            return table[i]
+        else:
+            False
 
+    """
+    This is going to check the columns
+    """
 
+    for j in range(0, 3, 1):
+        if table[j] == table[j + 3] == table[j + 6] == X or O:
+            return table[j]
+        else:
+            False
 
-a = [' ',  ' ',  ' ',  ' ',  ' ',   ' ',  ' ',  ' ',   ' ']
+    """
+    These two are going to check the diagonals
+    """
 
-table(a)
+    if table[0] == table[4] == table[8] == X or O:
+        return table[0]
+    else:
+        False
+
+    if table[2] == table[4] == table[6] == X or O:
+        return table[2]
+    else:
+        False
+
